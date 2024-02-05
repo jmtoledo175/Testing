06b5810109459fec9e36c93c86772972c32ca9d3
jmtoledo175 <juli.toledo175@gmail.com>
2024-02-05 15:03:15 -0300

    feat: adding exc

--- Ciclos-Iteraciones-Operadores/test.js
+++ Ciclos-Iteraciones-Operadores/test.js
@@ -1,6 +1,27 @@
-let ingresarNumero = parseInt(prompt("Ingresar Numero"))
 
-for (let i= 1; i<= 10; i++){
-    let resultado= ingresarNumero *i;
-    alert (ingresarNumero + " X "+ i + " = " + resultado)
-}
+// Algoritmo de multiplicación.
+// let ingresarNumero = parseInt(prompt("Ingresar Numero"))
+
+// for (let i= 1; i<= 10; i++){
+//     let resultado= ingresarNumero *i;
+//     alert (ingresarNumero + " X "+ i + " = " + resultado)
+// }
+
+// Algoritmo de turnos
+// for (let i= 1; i<= 20; i++){
+//     let ingresarNombre = prompt ("Ingresar nombre")
+
+//     alert ("Turno n°"+i+" Nombre: " + ingresarNombre)
+// }
+
+//SUMA DE NUMEROS PARES
+// var pares = 0;
+
+// for (var i = 1; i <= 20; i++) {
+//     // Verificar si el número es par usando el operador de módulo (%)
+//     if (i % 2 === 0) {
+//         pares += i;
+//     }
+// }
+
+// console.log(pares);
