c709addc29506718f53d971b123b0c61452b8ae5
jmtoledo175 <juli.toledo175@gmail.com>
2024-02-05 12:35:49 -0300

    first commit

---
+++ Ciclos-Iteraciones-Operadores/test.js
@@ -0,0 +1,6 @@
+let ingresarNumero = parseInt(prompt("Ingresar Numero"))
+
+for (let i= 1; i<= 10; i++){
+    let resultado= ingresarNumero *i;
+    alert (ingresarNumero + " X "+ i + " = " + resultado)
+}
